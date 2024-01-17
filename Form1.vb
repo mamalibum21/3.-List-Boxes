6f43cc907fb87f49d84498fae21e69ac4b1be7b0
Lenovo-User <Lenovo-User@DESKTOP-66DCH94>
2024-01-17 21:50:37 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'ListBox1.Items.Add("iPhone")
+
+        Dim myitem
+
+        myitem = InputBox("enter your fav phone")
+        ListBox1.Items.Add(myitem)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim myitem2
+        myitem2 = InputBox("enter the name of the phone to remove")
+
+        ListBox1.Items.Remove(myitem2)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Items.Remove(ListBox1.SelectedItem)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ListBox1.Items.Clear()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
